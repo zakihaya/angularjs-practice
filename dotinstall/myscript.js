41790b7a911e22c06b2d8f688ab30ee32d1c5c43
hayazaki <toru.hayazaki@gmail.com>
2014-11-19 20:55:29 +0900

    Controllerの実装

---
+++ dotinstall/myscript.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('mainCtrl', ['$scope', function($scope) {
+  $scope.users = [
+      {"name": "hayazaki", "score": 1},
+      {"name": "hayazaki2", "score": 2},
+      {"name": "hayazaki3", "score": 3},
+    ];
+}]);
